3040167354b191141673a93dbba7395f29199d35
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-08 16:46:01 +0430

    m

---
+++ lib/common/data/prefs_data.dart
@@ -0,0 +1,10 @@
+import 'package:mafia/data/model/role/prefs_data.dart';
+
+final prefsData = PrefsData(
+  1,
+  null,
+  true,
+  DateTime.now().millisecondsSinceEpoch,
+  DateTime.now().millisecondsSinceEpoch,
+  DateTime.now().millisecondsSinceEpoch,
+);
