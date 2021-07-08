9950ece0b61cc5b3e541b529f4743caac53a5a5b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-08 16:46:15 +0430

    +groups

---
+++ lib/common/data/group_list.dart
@@ -0,0 +1,22 @@
+import 'package:mafia/data/model/role/group.dart';
+
+final groupList = [
+  Group(
+    1,
+    "mafiaGroup",
+    DateTime.now().millisecondsSinceEpoch,
+    DateTime.now().millisecondsSinceEpoch,
+  ),
+  Group(
+    2,
+    "citizenGroup",
+    DateTime.now().millisecondsSinceEpoch,
+    DateTime.now().millisecondsSinceEpoch,
+  ),
+  Group(
+    3,
+    "independentGroup",
+    DateTime.now().millisecondsSinceEpoch,
+    DateTime.now().millisecondsSinceEpoch,
+  ),
+];
