fc256898e6b0112dffc0d55263edeb6ac7ee37ba
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-08 17:30:23 +0430

    +more abs

--- lib/data/db/dao/dao.dart
+++ lib/data/db/dao/dao.dart
@@ -1,11 +1,16 @@
 abstract class BaseDao<T>{
 
-  String get createTableQuery;
-
-  //abstract mapping methods
+  /// abstract mapping methods
   T fromMap(Map<String, dynamic> query);
   List<T> fromList(List<Map<String,dynamic>> query);
   Map<String, dynamic> toMap(T object);
+
+  /// abstract db manipulation methods
+  Future<int> insert(T data);
+  Future delete(T data);
+  Future<List<T>?>? getAllFromDb();
+  Future<T?>? getFromDb(int id);
+  Future update(T data);
 }
 
 const tableColumnCreatedDate = 'createdDate';
