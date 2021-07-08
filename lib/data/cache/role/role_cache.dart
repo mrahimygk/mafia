171edf7fdb3d5d2b2d0cf80c78217cccbba7e405
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-08 17:30:01 +0430

    +more

--- lib/data/cache/role/role_cache.dart
+++ lib/data/cache/role/role_cache.dart
@@ -1,9 +1,13 @@
 import 'package:mafia/data/model/role/role.dart';
 
 abstract class RoleCache {
-  Future<List<Role>>? getRoles();
+  Future<List<Role>?>? getRoles();
 
-  void putRoles(List<Role> roles);
+  Future<Role?>? getRole(int id);
+
+  void putRoles(List<Role>? roles);
+
+  void putRole(Role? role);
 }
 
 class RoleCacheImpl extends RoleCache {
@@ -14,12 +18,26 @@ class RoleCacheImpl extends RoleCache {
       roles == null ? null : Future.value(roles!.values.toList());
 
   @override
-  void putRoles(List<Role> roleList) {
+  void putRoles(List<Role>? roleList) {
     if (roles == null) {
       roles = Map<int, Role>();
     }
-    roleList.forEach((element) {
-      roles![element.id] = element;
+    roleList?.forEach((element) {
+      roles?[element.id] = element;
     });
+  }
+
+  @override
+  Future<Role?>? getRole(int id) =>
+      roles == null ? null : Future.value(roles![id]);
+
+  @override
+  void putRole(Role? role) {
+    if (roles == null) {
+      roles = Map<int, Role>();
+    }
+
+    if (role == null) return;
+    roles?[role.id] = role;
   }
 }
