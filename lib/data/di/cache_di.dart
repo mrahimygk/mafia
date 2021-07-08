8f4af8f6f65c7496de245989bab4604273383fc5
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-08 17:41:53 +0430

    +role cache

--- lib/data/di/cache_di.dart
+++ lib/data/di/cache_di.dart
@@ -1,8 +1,12 @@
 import 'package:mafia/app/di.dart';
 import 'package:mafia/data/cache/coin/coins_cache.dart';
 import 'package:mafia/data/cache/coin/exchange_rate_cache.dart';
+import 'package:mafia/data/cache/role/group_cache.dart';
+import 'package:mafia/data/cache/role/role_cache.dart';
 
 void registerCacheModule() {
   serviceLocator.registerSingleton<CoinsCache>(CoinsCacheImpl());
   serviceLocator.registerSingleton<ExchangeRateCache>(ExchangeRateCacheImpl());
+  serviceLocator.registerSingleton<RoleCache>(RoleCacheImpl());
+  serviceLocator.registerSingleton<GroupCache>(GroupCacheImpl());
 }
