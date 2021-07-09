338e9ea3060e0c7d0d611778d896e395f74b9a0f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 17:37:04 +0430

    +more lists

--- lib/feature/di.dart
+++ lib/feature/di.dart
@@ -1,7 +1,12 @@
 import 'package:mafia/feature/player/di.dart';
 import 'package:mafia/feature/role/di.dart';
 
+import 'group/di.dart';
+import 'lists/di.dart';
+
 void registerPageModules() {
   registerPlayerListPage();
   registerRoleListPage();
+  registerGroupListPage();
+  registerAllListPage();
 }
