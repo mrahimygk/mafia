1391df1ce2075d1b89c80a52b62b3158d28ae24b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 10:31:29 +0430

    +player list

---
+++ lib/feature/player/di.dart
@@ -0,0 +1,9 @@
+import 'package:mafia/app/di.dart';
+import 'package:mafia/domain/usecase/player/get_players_usecase.dart';
+
+import 'logic/player_list_cubit.dart';
+
+void registerPlayerListPage() {
+  serviceLocator.registerFactory<PlayerListCubit>(
+      () => PlayerListCubit(serviceLocator.get<GetPlayersUseCase>()));
+}
