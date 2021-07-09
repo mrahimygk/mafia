3df76a7aa1355a971abd9cef3ddcd6b257c35277
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 17:52:06 +0430

    +empty list

---
+++ lib/common/widgets/empty_list_widget.dart
@@ -0,0 +1,20 @@
+import 'package:easy_localization/easy_localization.dart' as localization;
+import 'package:flutter/material.dart';
+
+class EmptyListWidget extends StatelessWidget {
+  final String listSubject;
+  final VoidCallback onAddClick;
+
+  EmptyListWidget(this.listSubject, this.onAddClick);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text("${"noData".tr()}, ${listSubject.tr()}"),
+        IconButton(icon: Icon(Icons.add_circle_rounded), onPressed: () {})
+      ],
+    );
+  }
+}
