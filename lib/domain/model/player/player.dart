20327096aac0a3cd720386f1f399c1c212565033
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-10 00:05:00 +0430

    adding player scope to player

--- lib/domain/model/player/player.dart
+++ lib/domain/model/player/player.dart
@@ -1,12 +1,18 @@
+import 'package:mafia/domain/model/player/player_scope.dart';
+
 class Player {
   final int id;
   final String name;
+  final PlayerScope? scope;
+  bool isSelected = false;
   final int createdDate;
   final int modifiedDate;
 
   Player(
     this.id,
     this.name,
+    this.scope,
+    this.isSelected,
     this.createdDate,
     this.modifiedDate,
   );
@@ -14,12 +20,16 @@ class Player {
   Player copyWith({
     int? id,
     String? name,
+    PlayerScope? scope,
+    bool? isSelected,
     int? createdDate,
     int? modifiedDate,
   }) =>
       Player(
         id ?? this.id,
         name ?? this.name,
+        scope ?? this.scope,
+        isSelected ?? this.isSelected,
         createdDate ?? this.createdDate,
         modifiedDate ?? this.modifiedDate,
       );
