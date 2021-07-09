581b0906b06d313f95f14867f5060d7495d2ac8f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 10:50:42 +0430

    +dao

--- lib/data/db/di.dart
+++ lib/data/db/di.dart
@@ -1,5 +1,7 @@
 import 'package:mafia/app/di.dart';
 import 'package:mafia/data/db/dao/group_dao.dart';
+import 'package:mafia/data/db/dao/occupation_dao.dart';
+import 'package:mafia/data/db/dao/player_dao.dart';
 import 'package:mafia/data/db/dao/prefs_data_dao.dart';
 import 'package:mafia/data/db/dao/role_dao.dart';
 
@@ -14,6 +16,21 @@ void registerDbModule() {
   serviceLocator
       .registerSingleton(GroupDao(serviceLocator.get<DatabaseProvider>()));
 
-  serviceLocator
-      .registerSingleton(RoleDao(serviceLocator.get<DatabaseProvider>()));
+  serviceLocator.registerSingleton(
+    RoleDao(
+      serviceLocator.get<DatabaseProvider>(),
+      serviceLocator.get<GroupDao>(),
+    ),
+  );
+
+  serviceLocator.registerSingleton(
+    OccupationDao(
+      serviceLocator.get<DatabaseProvider>(),
+      serviceLocator.get<RoleDao>(),
+    ),
+  );
+
+  serviceLocator.registerSingleton(PlayerDao(
+    serviceLocator.get<DatabaseProvider>(),
+  ));
 }
