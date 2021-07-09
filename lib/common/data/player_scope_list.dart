2a1980fe3811e2c798298749c58b8fc4708175fd
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 23:41:53 +0430

    +player and scope list

---
+++ lib/common/data/player_scope_list.dart
@@ -0,0 +1,10 @@
+import 'package:mafia/data/model/player/player_scope.dart';
+
+final playerScopeList = [
+  PlayerScope(
+    1,
+    "Infinite8",
+    DateTime.now().millisecondsSinceEpoch,
+    DateTime.now().millisecondsSinceEpoch,
+  ),
+];
