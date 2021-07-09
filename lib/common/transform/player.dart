ed18648dbef83095eddc61c7083f5225b87a274e
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-10 00:04:50 +0430

    adding player scope to player

--- lib/common/transform/player.dart
+++ lib/common/transform/player.dart
@@ -1,10 +1,14 @@
 import 'package:mafia/data/model/player/player.dart' as data;
 import 'package:mafia/domain/model/player/player.dart' as domain;
 
+import './player_scope.dart';
+
 extension ToDomain on data.Player {
   domain.Player toDomain() => domain.Player(
         id,
         name,
+        scope?.toDomain(),
+        false,
         createdDate,
         modifiedDate,
       );
