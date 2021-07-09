4cbd1fc6760c5b4ad3e55652dab1be56e72ed4c3
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-10 00:05:28 +0430

    reusing role item widget

--- lib/feature/role/view/role_item_widget.dart
+++ lib/feature/role/view/role_item_widget.dart
@@ -1,9 +1,10 @@
 import 'package:easy_localization/easy_localization.dart' as localization;
 import 'package:flutter/material.dart';
+import 'package:mafia/domain/model/player/player.dart';
 import 'package:mafia/domain/model/role/role.dart';
 
-class RoleItemWidget extends StatefulWidget {
-  final Role item;
+class RoleItemWidget<T> extends StatefulWidget {
+  final T item;
 
   const RoleItemWidget({Key? key, required this.item}) : super(key: key);
 
@@ -12,7 +13,6 @@ class RoleItemWidget extends StatefulWidget {
 }
 
 class _RoleItemWidgetState extends State<RoleItemWidget> {
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -25,23 +25,29 @@ class _RoleItemWidgetState extends State<RoleItemWidget> {
         padding: const EdgeInsets.all(4.0),
         child: Container(
           decoration: BoxDecoration(
-            color: widget.item.group.id == 1
-                ? widget.item.isSelected
-                    ? Colors.red
-                    : Colors.red.shade100
-                : widget.item.group.id == 2
+            color: widget.item is Role
+                ? widget.item.group.id == 1
                     ? widget.item.isSelected
-                        ? Colors.green
-                        : Colors.green.shade100
-                    : widget.item.isSelected
-                        ? Colors.yellow
-                        : Colors.yellow.shade100,
+                        ? Colors.red
+                        : Colors.red.shade100
+                    : widget.item.group.id == 2
+                        ? widget.item.isSelected
+                            ? Colors.green
+                            : Colors.green.shade100
+                        : widget.item.isSelected
+                            ? Colors.yellow
+                            : Colors.yellow.shade100
+                : widget.item is Player
+                    ? widget.item.isSelected
+                        ? Colors.blueGrey
+                        : Colors.blueGrey.shade100
+                    : Colors.black,
             border: Border(),
             borderRadius: BorderRadius.all(Radius.circular(6.0)),
           ),
           padding: EdgeInsets.all(12.0),
           child: Text(
-            "${widget.item.name.tr()}",
+            "${widget.item is Role ? widget.item.name.toString().tr() : widget.item is Player ? widget.item.name : ""}",
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
