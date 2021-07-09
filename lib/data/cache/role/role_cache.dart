542b3123b90a78c12e9b61880f7bfc6cd52cc148
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 10:52:15 +0430

    +q by group

--- lib/data/cache/role/role_cache.dart
+++ lib/data/cache/role/role_cache.dart
@@ -3,6 +3,8 @@ import 'package:mafia/data/model/role/role.dart';
 abstract class RoleCache {
   Future<List<Role>?>? getRoles();
 
+  Future<List<Role>?>? getRolesByGroupId(int gid);
+
   Future<Role?>? getRole(int id);
 
   void putRoles(List<Role>? list);
@@ -16,6 +18,15 @@ class RoleCacheImpl extends RoleCache {
   @override
   Future<List<Role>>? getRoles() =>
       cache == null ? null : Future.value(cache!.values.toList());
+
+  @override
+  Future<List<Role>>? getRolesByGroupId(int gid) => cache == null
+      ? null
+      : Future.value(
+          cache!.values.where((element) {
+            return element.groupId == gid;
+          }).toList(),
+        );
 
   @override
   void putRoles(List<Role>? list) {
