9b48d0f33792c6865ab1bdba0eb2d3a75bf6e788
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 18:56:39 +0430

    separating player widget from full page

--- lib/feature/player/view/player_list_page.dart
+++ lib/feature/player/view/player_list_page.dart
@@ -1,106 +1,28 @@
 import 'package:easy_localization/easy_localization.dart' as localization;
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mafia/app/di.dart';
-import 'package:mafia/common/base/base_page.dart';
-import 'package:mafia/common/widgets/api_error_widget.dart';
-import 'package:mafia/common/widgets/empty_list_widget.dart';
-import 'package:mafia/data/model/player/player.dart';
-import 'package:mafia/feature/player/logic/player_list_cubit.dart';
+import 'package:mafia/common/widgets/drawer.dart';
+import 'package:mafia/feature/player/view/player_list_widget.dart';
 
-class PlayerListPage extends BasePage<PlayerListCubit, PlayerListState, void> {
-  final PlayerListCubit _cubit = serviceLocator.get<PlayerListCubit>();
-
+class PlayerListPage extends StatelessWidget {
   final VoidCallback onToggleTheme;
   final Function(int type) onDrawerItemClick;
 
   PlayerListPage(this.onToggleTheme, this.onDrawerItemClick, {Key? key})
-      : super(key: key) {
-    _cubit.getPlayerList();
-  }
+      : super(key: key);
 
   @override
-  Widget buildWidget(
-    BuildContext context,
-    TextDirection direction,
-    bool isDarkMode,
-  ) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("playerList".tr()),
-      ),
-      /*drawer: AppDrawer(
-        onToggleTheme: onToggleTheme,
-        onItemClick: onDrawerItemClick,
-        onLanguageChanged: (Locale? locale) {
-          localization.EasyLocalization.of(context)!.setLocale(locale!);
-        },
-      ),*/
-      body: BlocBuilder(
-        bloc: _cubit,
-        buildWhen: (previousState, currentState) {
-          return previousState != currentState;
-        },
-        builder: (BuildContext context, PlayerListState state) {
-          if (state is PlayerListInitialState ||
-              state is PlayerListNavigationState) {
-            return Container(color: Colors.white);
-          }
-
-          if (state is PlayerListLoadingState) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          if (state is PlayerListNoDataState) {
-            return Center(
-              child: EmptyListWidget("addPlayers", () {
-                //TODO: add players dialog
-              }),
-            );
-          }
-
-          if (state is PlayerListErrorState) {
-            return Center(
-              child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ApiErrorWidget(state.error, () {
-                    _cubit.getPlayerList();
-                  })),
-            );
-          }
-
-          if (state is PlayerListDataReceivedState) {
-            return _buildPlayerListView(state.players, direction, isDarkMode);
-          }
-
-          throw Exception("Please handle all states above $state");
-        },
-      ),
-    );
-  }
-
-  @override
-  PlayerListCubit getPageBloc() => _cubit;
-
-  Widget _buildPlayerListView(
-      List<Player> players, TextDirection direction, bool isDarkMode) {
-    return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          childAspectRatio: 0.5,
+        appBar: AppBar(
+          title: Text("playerList".tr()),
         ),
-        itemCount: players.length,
-        itemBuilder: (context, index) {
-          final item = players[index];
-          return GestureDetector(
-            onTapUp: (d) {
-              // _cubit.navigateToPlayerDetails(item);
-            },
-            child: Text(
-              "${item.name} (${item.id})",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          );
-        });
+        drawer: AppDrawer(
+          onToggleTheme: onToggleTheme,
+          onItemClick: onDrawerItemClick,
+          onLanguageChanged: (Locale? locale) {
+            localization.EasyLocalization.of(context)!.setLocale(locale!);
+          },
+        ),
+        body: PlayerListWidget(onToggleTheme, onDrawerItemClick));
   }
 }
---
+++ lib/feature/player/view/player_list_widget.dart
@@ -0,0 +1,109 @@
+import 'package:easy_localization/easy_localization.dart' as localization;
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mafia/app/di.dart';
+import 'package:mafia/common/base/base_page.dart';
+import 'package:mafia/common/widgets/api_error_widget.dart';
+import 'package:mafia/common/widgets/empty_list_widget.dart';
+import 'package:mafia/data/model/player/player.dart';
+import 'package:mafia/feature/player/logic/player_list_cubit.dart';
+
+class PlayerListWidget
+    extends BasePage<PlayerListCubit, PlayerListState, void> {
+  final PlayerListCubit _cubit = serviceLocator.get<PlayerListCubit>();
+
+  final VoidCallback onToggleTheme;
+  final Function(int type) onDrawerItemClick;
+
+  PlayerListWidget(this.onToggleTheme, this.onDrawerItemClick, {Key? key})
+      : super(key: key) {
+    _cubit.getPlayerList();
+  }
+
+  @override
+  Widget buildWidget(
+    BuildContext context,
+    TextDirection direction,
+    bool isDarkMode,
+  ) {
+    return BlocBuilder(
+        bloc: _cubit,
+        buildWhen: (previousState, currentState) {
+          return previousState != currentState;
+        },
+        builder: (BuildContext context, PlayerListState state) {
+          if (state is PlayerListInitialState ||
+              state is PlayerListNavigationState) {
+            return Container(color: Colors.white);
+          }
+
+          if (state is PlayerListLoadingState) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (state is PlayerListNoDataState) {
+            return Center(
+              child: EmptyListWidget("addPlayers", () {
+                //TODO: add players dialog
+              }),
+            );
+          }
+
+          if (state is PlayerListErrorState) {
+            return Center(
+              child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ApiErrorWidget(state.error, () {
+                    _cubit.getPlayerList();
+                  })),
+            );
+          }
+
+          if (state is PlayerListDataReceivedState) {
+            return _buildPlayerListView(state.players, direction, isDarkMode);
+          }
+
+          throw Exception("Please handle all states above $state");
+        },
+      );
+  }
+
+  @override
+  PlayerListCubit getPageBloc() => _cubit;
+
+  Widget _buildPlayerListView(
+      List<Player> players, TextDirection direction, bool isDarkMode) {
+    return SingleChildScrollView(
+      child: Wrap(
+        children: _buildRoleChildren(players, direction, isDarkMode),
+      ),
+    );
+  }
+
+  List<Widget> _buildRoleChildren(
+      List<Player> roles, TextDirection direction, bool isDarkMode) {
+    final List<Widget> list = [];
+    roles.forEach((item) {
+      list.add(GestureDetector(
+        onTapUp: (d) {},
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.blueGrey.shade100,
+              border: Border(),
+              borderRadius: BorderRadius.all(Radius.circular(6.0)),
+            ),
+            padding: EdgeInsets.all(6.0),
+            child: Text(
+              "${item.name}",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ));
+    });
+
+    return list;
+  }
+}
