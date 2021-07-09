8cd59acef2f2942f663be8e4db35fda019af0c2a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 23:09:19 +0430

    +scope

--- lib/data/model/player/player.dart
+++ lib/data/model/player/player.dart
@@ -1,15 +1,20 @@
 import 'package:mafia/data/db/dao/dao.dart';
 import 'package:mafia/data/db/dao/player_dao.dart';
+import 'package:mafia/data/model/player/player_scope.dart';
 
 class Player {
   final int id;
   final String name;
+  final int scopeId;
+  final PlayerScope? scope;
   final int createdDate;
   final int modifiedDate;
 
   Player(
     this.id,
     this.name,
+    this.scopeId,
+    this.scope,
     this.createdDate,
     this.modifiedDate,
   );
@@ -17,12 +22,16 @@ class Player {
   Player copyWith({
     int? id,
     String? name,
+    int? scopeId,
+    PlayerScope? scope,
     int? createdDate,
     int? modifiedDate,
   }) =>
       Player(
         id ?? this.id,
         name ?? this.name,
+        scopeId ?? this.scopeId,
+        scope ?? this.scope,
         createdDate ?? this.createdDate,
         modifiedDate ?? this.modifiedDate,
       );
@@ -31,6 +40,8 @@ class Player {
     return Player(
       json[playerColumnId],
       json[playerColumnName],
+      json[playerColumnScope],
+      null,
       json[tableColumnCreatedDate],
       json[tableColumnModifiedDate],
     );
@@ -40,6 +51,7 @@ class Player {
     final data = Map<String, dynamic>();
     data[playerColumnId] = id;
     data[playerColumnName] = name;
+    data[playerColumnScope] = scopeId;
     data[tableColumnCreatedDate] = createdDate;
     data[tableColumnModifiedDate] = modifiedDate;
     return data;
