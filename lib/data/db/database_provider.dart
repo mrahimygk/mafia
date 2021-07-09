f896553dda12f85c3d3f9b37d3bd109882025fbf
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-09 23:42:53 +0430

    inserting pre-defined players

--- lib/data/db/database_provider.dart
+++ lib/data/db/database_provider.dart
@@ -1,10 +1,13 @@
 
 import 'package:mafia/common/data/group_list.dart';
+import 'package:mafia/common/data/player_list.dart';
+import 'package:mafia/common/data/player_scope_list.dart';
 import 'package:mafia/common/data/prefs_data.dart';
 import 'package:mafia/common/data/role_list.dart';
 import 'package:mafia/data/db/dao/group_dao.dart';
 import 'package:mafia/data/db/dao/occupation_dao.dart';
 import 'package:mafia/data/db/dao/player_dao.dart';
+import 'package:mafia/data/db/dao/player_scope_dao.dart';
 import 'package:mafia/data/db/dao/role_dao.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -32,6 +35,7 @@ class DatabaseProvider {
         onCreate: (Database db, int version) async {
       await db.execute(PrefsDataDao.createTableQuery);
       await db.execute(GroupDao.createTableQuery);
+      await db.execute(PlayerScopeDao.createTableQuery);
       await db.execute(RoleDao.createTableQuery);
       await db.execute(PlayerDao.createTableQuery);
       await db.execute(OccupationDao.createTableQuery);
@@ -44,8 +48,16 @@ class DatabaseProvider {
         batch.insert(groupTable, element.toMap());
       });
 
+      playerScopeList.forEach((element) {
+        batch.insert(playerScopeTable, element.toMap());
+      });
+
       roleList.forEach((element) {
         batch.insert(roleTable, element.toMap());
+      });
+
+      playerList.forEach((element) {
+        batch.insert(playerTable, element.toMap());
       });
 
       batch.commit();
