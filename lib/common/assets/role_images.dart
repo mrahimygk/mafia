4774cfa829be1d0905e69a8e3f727629d5b805ea
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-10 15:37:02 +0430

    +role images

--- lib/common/assets/role_images.dart
+++ lib/common/assets/role_images.dart
@@ -1,4 +1,4 @@
-const String _rolesPath = "assets/roles";
+const String rolesPath = "assets/roles";
 
-const String mafiaImage = "$_rolesPath/mafia.png";
-const String godfatherImage = "$_rolesPath/godfather.png";
+const String mafiaImage = "$rolesPath/mafia.png";
+const String godfatherImage = "$rolesPath/godfather.png";
