c931096829c7e4a41115bcfaaaa4a4cb620986fd
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-12 21:28:55 +0430

    producing new players state for view

--- lib/feature/player/logic/player_list_cubit.dart
+++ lib/feature/player/logic/player_list_cubit.dart
@@ -3,6 +3,7 @@ import 'package:mafia/common/base/page_state.dart';
 import 'package:mafia/domain/model/base/api_resource.dart';
 import 'package:mafia/domain/model/base/status.dart';
 import 'package:mafia/domain/model/player/player.dart';
+import 'package:mafia/domain/usecase/player/get_players_by_ids_usecase.dart';
 import 'package:mafia/domain/usecase/player/get_players_usecase.dart';
 import 'package:mafia/navigation/type.dart';
 
@@ -10,8 +11,12 @@ part 'player_list_state.dart';
 
 class PlayerListCubit extends PageCubit<PlayerListState> {
   final GetPlayersUseCase _getPlayersUseCase;
+  final GetPlayersByIdsUseCase _getPlayersByIdsUseCase;
 
-  PlayerListCubit(this._getPlayersUseCase) : super(PlayerListInitialState());
+  List<Player>? cachedPlayers;
+
+  PlayerListCubit(this._getPlayersUseCase, this._getPlayersByIdsUseCase)
+      : super(PlayerListInitialState());
 
   void getPlayerList() {
     _getPlayersUseCase.execute(null).listen((ApiResource<List<Player>> event) {
@@ -24,6 +29,7 @@ class PlayerListCubit extends PageCubit<PlayerListState> {
           if (event.data == null || (event.data?.isEmpty == true)) {
             emit(PlayerListNoDataState());
           } else {
+            cachedPlayers = event.data!;
             emit(PlayerListDataReceivedState(event.data!));
           }
           break;
@@ -38,5 +44,33 @@ class PlayerListCubit extends PageCubit<PlayerListState> {
 
   void navigateToPlayerDetails(Player player) {
     //emit(PlayerListNavigationState(NavigationRoutes.DETAILS, player));
+  }
+
+  void checkForNewPlayers(Set<int> insertedIds) {
+    cachedPlayers == null || cachedPlayers!.isEmpty
+        ? getPlayerList()
+        : _getPlayersByIdsUseCase
+            .execute(insertedIds.toList())
+            .listen((ApiResource<List<Player>> event) {
+            switch (event.status) {
+              case Status.LOADING:
+                emit(PlayerListLoadingState());
+                break;
+
+              case Status.SUCCESS:
+                if (event.data == null) {
+                  emit(PlayerListDataReceivedState(cachedPlayers!));
+                } else {
+                  cachedPlayers!.addAll(event.data!);
+                  emit(PlayerListDataReceivedState(cachedPlayers!));
+                }
+                break;
+
+              case Status.ERROR:
+                emit(PlayerListErrorState(event.message!));
+            }
+          }).onError((e, s) {
+            emit(PlayerListErrorState(e.toString()));
+          });
   }
 }
