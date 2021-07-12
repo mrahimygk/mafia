3af759c312f5d54e330a3bc30b4dffc35336df5f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-12 23:39:05 +0430

    using DeletePlayersByIdsUseCase to remove player from db and produce PlayerListDataRemovedState

--- lib/feature/player/logic/player_list_cubit.dart
+++ lib/feature/player/logic/player_list_cubit.dart
@@ -3,6 +3,7 @@ import 'package:mafia/common/base/page_state.dart';
 import 'package:mafia/domain/model/base/api_resource.dart';
 import 'package:mafia/domain/model/base/status.dart';
 import 'package:mafia/domain/model/player/player.dart';
+import 'package:mafia/domain/usecase/player/delete_players_by_ids_usecase.dart';
 import 'package:mafia/domain/usecase/player/get_players_by_ids_usecase.dart';
 import 'package:mafia/domain/usecase/player/get_players_usecase.dart';
 import 'package:mafia/navigation/type.dart';
@@ -12,11 +13,15 @@ part 'player_list_state.dart';
 class PlayerListCubit extends PageCubit<PlayerListState> {
   final GetPlayersUseCase _getPlayersUseCase;
   final GetPlayersByIdsUseCase _getPlayersByIdsUseCase;
+  final DeletePlayersByIdsUseCase _deletePlayersByIdsUseCase;
 
   List<Player>? cachedPlayers;
 
-  PlayerListCubit(this._getPlayersUseCase, this._getPlayersByIdsUseCase)
-      : super(PlayerListInitialState());
+  PlayerListCubit(
+    this._getPlayersUseCase,
+    this._getPlayersByIdsUseCase,
+    this._deletePlayersByIdsUseCase,
+  ) : super(PlayerListInitialState());
 
   void getPlayerList() {
     _getPlayersUseCase.execute(null).listen((ApiResource<List<Player>> event) {
@@ -72,5 +77,23 @@ class PlayerListCubit extends PageCubit<PlayerListState> {
           }).onError((e, s) {
             emit(PlayerListErrorState(e.toString()));
           });
+  }
+
+  void removePlayer(Player item) {
+    _deletePlayersByIdsUseCase.execute([item.id]).listen((event) {
+      switch (event.status) {
+        case Status.LOADING:
+          emit(PlayerListLoadingState());
+          break;
+
+        case Status.SUCCESS:
+          cachedPlayers?.remove(item);
+          emit(PlayerListDataRemovedState(cachedPlayers!));
+          break;
+
+        case Status.ERROR:
+          emit(PlayerListErrorState(event.message!));
+      }
+    });
   }
 }
