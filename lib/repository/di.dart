4ff4489c60eb5ba229bbe3ce1f4e090e2d8af9c2
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-15 21:27:06 +0430

    declaring occupation and game repo

--- lib/repository/di.dart
+++ lib/repository/di.dart
@@ -1,10 +1,16 @@
 import 'package:mafia/app/di.dart';
+import 'package:mafia/data/cache/game/game_cache.dart';
+import 'package:mafia/data/cache/occupation/occupation_cache.dart';
 import 'package:mafia/data/cache/player/player_cache.dart';
 import 'package:mafia/data/cache/role/group_cache.dart';
 import 'package:mafia/data/cache/role/role_cache.dart';
+import 'package:mafia/data/db/dao/game_dao.dart';
 import 'package:mafia/data/db/dao/group_dao.dart';
+import 'package:mafia/data/db/dao/occupation_dao.dart';
 import 'package:mafia/data/db/dao/player_dao.dart';
 import 'package:mafia/data/db/dao/role_dao.dart';
+import 'package:mafia/repository/game/game_repository.dart';
+import 'package:mafia/repository/occupation/occupation_repository.dart';
 import 'package:mafia/repository/player/player_repository.dart';
 import 'package:mafia/repository/role/group_repository.dart';
 import 'package:mafia/repository/role/role_repository.dart';
@@ -23,5 +29,15 @@ void registerRepositoryModule() {
   serviceLocator.registerFactory<PlayerRepository>(() => PlayerRepositoryImpl(
         serviceLocator.get<PlayerDao>(),
         serviceLocator.get<PlayerCache>(),
+      ));
+
+  serviceLocator.registerFactory<OccupationRepository>(() => OccupationRepositoryImpl(
+        serviceLocator.get<OccupationDao>(),
+        serviceLocator.get<OccupationCache>(),
+      ));
+
+  serviceLocator.registerFactory<GameRepository>(() => GameRepositoryImpl(
+        serviceLocator.get<GameDao>(),
+        serviceLocator.get<GameCache>(),
       ));
 }
