7a846a54c4e1b189c8eda9ce752bca52609c32c8
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-15 21:19:42 +0430

    declaring game and cast cache

--- lib/data/di/cache_di.dart
+++ lib/data/di/cache_di.dart
@@ -1,4 +1,6 @@
 import 'package:mafia/app/di.dart';
+import 'package:mafia/data/cache/game/game_cache.dart';
+import 'package:mafia/data/cache/occupation/occupation_cache.dart';
 import 'package:mafia/data/cache/player/player_cache.dart';
 import 'package:mafia/data/cache/role/group_cache.dart';
 import 'package:mafia/data/cache/role/role_cache.dart';
@@ -7,4 +9,6 @@ void registerCacheModule() {
   serviceLocator.registerSingleton<GroupCache>(GroupCacheImpl());
   serviceLocator.registerSingleton<RoleCache>(RoleCacheImpl());
   serviceLocator.registerSingleton<PlayerCache>(PlayerCacheImpl());
+  serviceLocator.registerSingleton<OccupationCache>(OccupationCacheImpl());
+  serviceLocator.registerSingleton<GameCache>(GameCacheImpl());
 }
