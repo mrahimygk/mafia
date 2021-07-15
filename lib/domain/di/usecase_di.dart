cb19db56d7ae2cb4aef7db05da5579249dac20f5
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-15 21:20:22 +0430

    declaring occ and game usecase

--- lib/domain/di/usecase_di.dart
+++ lib/domain/di/usecase_di.dart
@@ -1,4 +1,8 @@
 import 'package:mafia/app/di.dart';
+import 'package:mafia/domain/usecase/game/get_game_by_id_usecase.dart';
+import 'package:mafia/domain/usecase/game/insert_game_usecase.dart';
+import 'package:mafia/domain/usecase/game/new_game_usecase.dart';
+import 'package:mafia/domain/usecase/occupation/get_occupation_list_usecase.dart';
 import 'package:mafia/domain/usecase/player/delete_players_by_ids_usecase.dart';
 import 'package:mafia/domain/usecase/player/get_players_by_ids_usecase.dart';
 import 'package:mafia/domain/usecase/player/get_players_usecase.dart';
@@ -7,6 +11,8 @@ import 'package:mafia/domain/usecase/role/get_group_by_id_usecase.dart';
 import 'package:mafia/domain/usecase/role/get_groups_usecase.dart';
 import 'package:mafia/domain/usecase/role/get_role_by_id_usecase.dart';
 import 'package:mafia/domain/usecase/role/get_roles_usecase.dart';
+import 'package:mafia/repository/game/game_repository.dart';
+import 'package:mafia/repository/occupation/occupation_repository.dart';
 import 'package:mafia/repository/player/player_repository.dart';
 import 'package:mafia/repository/role/group_repository.dart';
 import 'package:mafia/repository/role/role_repository.dart';
@@ -33,6 +39,18 @@ void registerUseCaseModule() {
   serviceLocator.registerFactory<InsertPlayerUseCase>(
       () => InsertPlayerUseCaseImpl(serviceLocator.get<PlayerRepository>()));
 
-  serviceLocator.registerFactory<DeletePlayersByIdsUseCase>(
-      () => DeletePlayersByIdsUseCaseImpl(serviceLocator.get<PlayerRepository>()));
+  serviceLocator.registerFactory<DeletePlayersByIdsUseCase>(() =>
+      DeletePlayersByIdsUseCaseImpl(serviceLocator.get<PlayerRepository>()));
+
+  serviceLocator.registerFactory<GetOccupationsUseCase>(() =>
+      GetOccupationsUseCaseImpl(serviceLocator.get<OccupationRepository>()));
+
+  serviceLocator.registerFactory<GetGameByIdUseCase>(
+      () => GetGameByIdUseCaseImpl(serviceLocator.get<GameRepository>()));
+
+  serviceLocator.registerFactory<InsertGameUseCase>(
+      () => InsertGameUseCaseImpl(serviceLocator.get<GameRepository>()));
+
+  serviceLocator.registerFactory<NewGameUseCase>(
+      () => NewGameUseCaseImpl(serviceLocator.get<GameRepository>()));
 }
