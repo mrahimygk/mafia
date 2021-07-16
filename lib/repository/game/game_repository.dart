6439c2bcd8236d9a157a850f704307a6401a71e4
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-16 21:20:11 +0430

    implementing the basic algorithm to toss roles

--- lib/repository/game/game_repository.dart
+++ lib/repository/game/game_repository.dart
@@ -119,26 +119,117 @@ class GameRepositoryImpl extends GameRepository {
        * inserting 3 normal players for each mafia
        */
       final List<Occupation> occupations = [];
-      int index = 0;
+      int index = 1;
 
       players.forEach((player) {
-        if (index != 0 && index % 3 == 0) {
+        if (index != 1 && index % 4 == 0) {
+          Role negativeRole =
+              roles.where((element) => element.name == 'mafia').first;
+          if (index > 4) {
+            if (occupations.where((Occupation element) {
+                  return element.roleId == 2; // 2 is godfather
+                }).isEmpty &&
+                roles.where((element) {
+                  return element.name == 'godfather';
+                }).isNotEmpty) {
+              negativeRole = roles.where((element) {
+                return element.name == 'godfather';
+              }).first;
+            }
+
+            if (occupations.where((Occupation element) {
+              return element.roleId == 3; // 3 is negotiator
+            }).isEmpty &&
+                roles.where((element) {
+                  return element.name == 'negotiator';
+                }).isNotEmpty && negativeRole.name != 'godfather') {
+              negativeRole = roles.where((element) {
+                return element.name == 'negotiator';
+              }).first;
+            }
+
+            if (occupations.where((Occupation element) {
+              return element.roleId == 4; // 4 is drLecter
+            }).isEmpty &&
+                roles.where((element) {
+                  return element.name == 'drLecter';
+                }).isNotEmpty && negativeRole.name != 'negotiator'&& negativeRole.name != 'godfather') {
+              negativeRole = roles.where((element) {
+                return element.name == 'drLecter';
+              }).first;
+            }
+          }
+
           occupations.add(Occupation(
             -1,
             gameId,
             player.id,
-            roles.where((element) => element.name == 'mafia').first.id,
+            negativeRole.id,
             null,
             null,
             DateTime.now().millisecondsSinceEpoch,
             DateTime.now().millisecondsSinceEpoch,
           ));
         } else {
+          Role positiveRole =
+              roles.where((element) => element.name == 'citizen').first;
+          if (index == 2) {
+            if (occupations.where((Occupation element) {
+                  return element.roleId == 18; // 18 is medic
+                }).isEmpty &&
+                roles.where((element) {
+                  return element.name == 'medic';
+                }).isNotEmpty) {
+              positiveRole = roles.where((element) {
+                return element.name == 'medic';
+              }).first;
+            }
+          }
+
+          if (index == 3) {
+            if (occupations.where((Occupation element) {
+                  return element.roleId == 19; // 19 is sniper
+                }).isEmpty &&
+                roles.where((element) {
+                  return element.name == 'sniper';
+                }).isNotEmpty) {
+              positiveRole = roles.where((element) {
+                return element.name == 'sniper';
+              }).first;
+            }
+          }
+
+          if (index == 5) {
+            if (occupations.where((Occupation element) {
+                  return element.roleId == 20; // 20 is detective
+                }).isEmpty &&
+                roles.where((element) {
+                  return element.name == 'detective';
+                }).isNotEmpty) {
+              positiveRole = roles.where((element) {
+                return element.name == 'detective';
+              }).first;
+            }
+          }
+
+          if (index == 9) {
+            if (occupations.where((Occupation element) {
+                  return element.roleId == 21; // 21 is mayor
+                }).isEmpty &&
+                roles.where((element) {
+                  return element.name == 'mayor';
+                }).isNotEmpty) {
+              positiveRole = roles.where((element) {
+                return element.name == 'mayor';
+              }).first;
+            }
+          }
+
           occupations.add(Occupation(
             -1,
             gameId,
             player.id,
-            roles.where((element) => element.group.id == 2).first.id,
+            positiveRole.id,
             null,
             null,
             DateTime.now().millisecondsSinceEpoch,
