469ea0483f1019adfe2d8b57421915fc31f7bb6c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-16 14:27:39 +0430

    +data class to pass to usecases

---
+++ lib/domain/model/player/occupation_insert_request.dart
@@ -0,0 +1,13 @@
+
+
+class OccupationInsertRequest {
+  final int gameId;
+  final int playerId;
+  final int roleId;
+
+  OccupationInsertRequest(
+    this.gameId,
+    this.playerId,
+    this.roleId,
+  );
+}
