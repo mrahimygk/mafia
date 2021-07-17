7732543a040267a7005dc3ac53cb1ee747637dd0
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-17 19:46:23 +0430

    removing scaffold

--- lib/feature/lists/view/all_list_page.dart
+++ lib/feature/lists/view/all_list_page.dart
@@ -20,52 +20,40 @@ class AllListPage extends BasePage<AllListCubit, AllListState, void> {
     TextDirection direction,
     bool isDarkMode,
   ) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("allLists".tr()),
-      ),
-      /*drawer: AppDrawer(
-        onToggleTheme: onToggleTheme,
-        onItemClick: onDrawerItemClick,
-        onLanguageChanged: (Locale? locale) {
-          localization.EasyLocalization.of(context)!.setLocale(locale!);
-        },
-      ),*/
-      body: BlocBuilder(
-        bloc: _cubit,
-        buildWhen: (previousState, currentState) {
-          return previousState != currentState;
-        },
-        builder: (BuildContext context, AllListState state) {
-          if (state is AllListInitialState || state is AllListNavigationState) {
-            return Container(color: Colors.white);
-          }
+    return BlocBuilder(
+      bloc: _cubit,
+      buildWhen: (previousState, currentState) {
+        return previousState != currentState;
+      },
+      builder: (BuildContext context, AllListState state) {
+        if (state is AllListInitialState || state is AllListNavigationState) {
+          return Container(color: Colors.white);
+        }
 
-          if (state is AllListLoadingState) {
-            return Center(child: CircularProgressIndicator());
-          }
+        if (state is AllListLoadingState) {
+          return Center(child: CircularProgressIndicator());
+        }
 
-          if (state is AllListNoDataState) {
-            return Text("No data, maybe need to reinstall app or report a bug");
-          }
+        if (state is AllListNoDataState) {
+          return Text("No data, maybe need to reinstall app or report a bug");
+        }
 
-          if (state is AllListErrorState) {
-            return Center(
-              child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ApiErrorWidget(state.error, () {
-                    _cubit.getAllList();
-                  })),
-            );
-          }
+        if (state is AllListErrorState) {
+          return Center(
+            child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ApiErrorWidget(state.error, () {
+                  _cubit.getAllList();
+                })),
+          );
+        }
 
-          if (state is AllListDataReceivedState) {
-            return _buildAllListView(state.listItems, direction, isDarkMode);
-          }
+        if (state is AllListDataReceivedState) {
+          return _buildAllListView(state.listItems, direction, isDarkMode);
+        }
 
-          throw Exception("Please handle all states above $state");
-        },
-      ),
+        throw Exception("Please handle all states above $state");
+      },
     );
   }
 
